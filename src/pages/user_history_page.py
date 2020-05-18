9a17f4f25015dd4454509aadc1cc4035ba449bc2
Helen Kravchuk <kravchukl2000@gmail.com>
2020-05-19 00:54:04 +0300

    add default option, add delete history

--- src/pages/user_history_page.py
+++ src/pages/user_history_page.py
@@ -5,17 +5,21 @@ from db.db import *
 
 def user_history_page():
     st.title('User history')
+
+    if st.sidebar.button("Delete history"):
+        db_clean()
+
     rows = db_select()
 
     col = ('Modification','Excecution time', 'Insert date', 'Hypothesis', 'Cost function', 'Regularization','Scalling function',
     'Reg coef', 'Learning rate', 'Early stopping', 'Number of itterations', 'Weights')
 
-    df = pd.DataFrame(
-    rows,
-    columns=col)
+    df = pd.DataFrame(rows, columns=col)
 
-    columns = st.multiselect(
-    label='What information do you want to display?', options=df.columns)
+    columns = st.multiselect(label='What information do you want to display?', options=col, default=list(col))
 
-    if columns:
-        st.table(df[columns])
+    if columns and df.shape[0]:
+        st.write(df[columns])
+    else:
+        st.warning('User history is empty')
+                      
